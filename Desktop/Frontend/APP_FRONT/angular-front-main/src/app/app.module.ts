77c118a618d23f6ae1494bd1ba5adccb8fb98292
Padawan2612 <mjoeln2612@hotmail.com>
2022-07-10 21:18:15 -0500

    front

---
+++ Desktop/Frontend/APP_FRONT/angular-front-main/src/app/app.module.ts
@@ -0,0 +1,20 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HttpClient } from '@angular/common/http';
+@NgModule({
+  declarations: [
+    AppComponent,
+       ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClient
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
